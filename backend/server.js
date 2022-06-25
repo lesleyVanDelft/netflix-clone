69fde0810f8d02d1a5c9076dfcf6fc79586fe37e
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-06-25 13:58:45 +0200

    heroku test

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,22 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/media', mediaRoutes);
 
-app.get('/api/media/all', async (req, res) => {});
+// app.get('/api/media/all', async (req, res) => {});
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+	app.get('/*', (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+		);
+	});
+} else {
+	app.get('/', (req, res) => {
+		res.send('API is running');
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`Netflixclone is running on port ${PORT}`);
