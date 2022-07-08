3fcbadac6df6009485f975a5dcef6d6c612b8c8c
Lesley van Delft <lesleyvandelft92@gmail.com>
2022-07-08 17:57:42 +0200

    add swiper to manage profiles page

--- frontend/src/pages/Profile/ManageProfiles.js
+++ frontend/src/pages/Profile/ManageProfiles.js
@@ -1,17 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
-import {
-	reset,
-	resetUpdateStatus,
-	selectProfile,
-} from '../../features/userSlice/userSlice';
+// import {reset} from '../../features/userSlice/userSlice';
 import { ReactComponent as Logo } from '../../assets/logo.svg';
 import BlankProfilePic from '../../assets/blank-profile-picture.png';
 import { FaPlusCircle } from 'react-icons/fa';
 import { TiPencil } from 'react-icons/ti';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import ManageModal from './ManageModal';
+import { useMediaQuery } from 'react-responsive';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/scrollbar';
 
 const ManageProfiles = () => {
 	const [editActive, setEditActive] = useState(false);
@@ -21,16 +25,10 @@ const ManageProfiles = () => {
 	const navigate = useNavigate();
 	const dispatch = useDispatch();
 
-	const handleReset = () => {
-		return reset(null);
-	};
+	const isDesktop = useMediaQuery({
+		query: '(min-width: 1024px)',
+	});
 
-	// useEffect(() => {
-	// 	// updateStatus !== null && handleReset();
-	// 	// console.log(updateStatus);
-	// 	// updateStatus !== (null || 'updatePending') && dispatch(resetUpdateStatus(user.updateStatus));
-	// 	dispatch(resetUpdateStatus(user.updateStatus));
-	// }, [user.updateStatus]);
 	return (
 		<main className="ManageProfiles">
 			<Logo className="pageLogo" />
@@ -44,40 +42,85 @@ const ManageProfiles = () => {
 			</AnimatePresence>
 			<h1>Manage Profiles:</h1>
 			<div className="ManageProfiles__users">
-				{user.profiles &&
-					user.profiles.map((prof, i) => {
-						return (
-							<div
-								className="user"
-								key={i}
-								onClick={() => {
-									setEditActive(true);
-									setProfileData(prof);
-								}}>
-								<div className="avatar-wrapper">
-									<img
-										src={
-											prof.profileImage.exists
-												? prof.profileImage.imageId
-												: BlankProfilePic
-										}
-										alt="user profile avatar"
-									/>
-									<TiPencil className="editSvg" />
+				{isDesktop && user !== null && user.profiles && (
+					<>
+						{user.profiles.map((prof, i) => {
+							return (
+								<div
+									className="user"
+									key={i}
+									onClick={() => {
+										setEditActive(true);
+										setProfileData(prof);
+									}}>
+									<div className="avatar-wrapper">
+										<img
+											src={
+												prof.profileImage.exists
+													? prof.profileImage.imageId
+													: BlankProfilePic
+											}
+											alt="user profile avatar"
+										/>
+										<TiPencil className="editSvg" />
+									</div>
+
+									<span className="user__profileName">{prof.username}</span>
 								</div>
-
-								<span className="user__profileName">{prof.username}</span>
+							);
+						})}
+						<div className="user add" onClick={null}>
+							<div className="icon">
+								<FaPlusCircle />
 							</div>
-						);
-					})}
+							<span className="user__profileName">Add profile</span>
+						</div>
+					</>
+				)}
 
-				{/* <div className="Profile__users--create"> */}
-				<div className="user add" onClick={null}>
-					<div className="icon">
-						<FaPlusCircle />
-					</div>
-					<span className="user__profileName">Add profile</span>
-				</div>
+				{!isDesktop && user !== null && user.profiles && (
+					<Swiper
+						spaceBetween={16}
+						modules={[Pagination, Scrollbar, A11y]}
+						slidesPerView={2}
+						preloadImages={true}
+						scrollbar={{ draggable: true }}
+						loop={false}>
+						{user.profiles.map((prof, i) => {
+							return (
+								<SwiperSlide key={i}>
+									<div
+										className="user"
+										onClick={() => {
+											setEditActive(true);
+											setProfileData(prof);
+										}}>
+										<div className="avatar-wrapper">
+											<img
+												src={
+													prof.profileImage.exists
+														? prof.profileImage.imageId
+														: BlankProfilePic
+												}
+												alt="user profile avatar"
+											/>
+											<TiPencil className="editSvg" />
+										</div>
+										<span className="user__profileName">{prof.username}</span>
+									</div>
+								</SwiperSlide>
+							);
+						})}
+						<SwiperSlide>
+							<div className="user add" onClick={null}>
+								<div className="icon">
+									<FaPlusCircle />
+								</div>
+								<span className="user__profileName">Add profile</span>
+							</div>
+						</SwiperSlide>
+					</Swiper>
+				)}
 			</div>
 
 			<button className="btn btn--profilePage" onClick={() => navigate('/')}>
